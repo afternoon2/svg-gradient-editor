c8d3f7afb2cf3b4014a13fae1218045f3cbc1753
Jakub Antolak <poprostuantolak@gmail.com>
2018-12-01 11:40:15 +0100

    add fonts

--- src/components/layout/GlobalStyles.ts
+++ src/components/layout/GlobalStyles.ts
@@ -1,13 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
+import { fontFace } from '../../styles/fontFace';
 
 export const GlobalStyles = createGlobalStyle`
+
+  ${fontFace};
+
   body {
     margin: 0;
     padding: 0;
+    font-family: 'Open Sans', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
   code {
-    font-family: 'Fira Code', source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+    font-family: 'Fira Mono', source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
 `;
