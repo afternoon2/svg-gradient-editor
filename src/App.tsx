8fc46dc5df21946479fa101dd790ea43460f674f
Jakub Antolak <poprostuantolak@gmail.com>
2024-08-18 12:58:03 +0200

    use vite, upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,5 @@
-import * as React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faChevronLeft,
-  faList,
-  faPaintRoller,
-  faCaretDown,
-  faCaretUp,
-  faPlus,
-  faSearch,
-  faTrash,
-  faEdit,
-  faCode,
-  faSave,
-  faTimes,
-} from '@fortawesome/free-solid-svg-icons';
-import { StoreThemeProvider } from './components/layout/StoreThemeProvider';
-import { Editor } from './views/editor/Editor';
-import { GlobalStyles } from './components/layout/GlobalStyles';
-import { ThemeSwitch } from './components/layout/ThemeSwitch';
+function App() {
+  return <>Hello world</>;
+}
 
-library.add(faChevronLeft);
-library.add(faList);
-library.add(faPaintRoller);
-library.add(faCaretDown);
-library.add(faCaretUp);
-library.add(faPlus);
-library.add(faSearch);
-library.add(faTrash);
-library.add(faEdit);
-library.add(faCode);
-library.add(faSave);
-library.add(faTimes);
-
-const App = () => (
-  <Provider store={store}>
-    <StoreThemeProvider>
-        <React.Fragment>
-          <Editor />
-          <ThemeSwitch />
-          <GlobalStyles />
-        </React.Fragment>
-    </StoreThemeProvider>
-  </Provider>
-);
-
-export { App };
+export default App;
--- src/store/reducers.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { application } from './application/reducer';
-import { editor } from './editor/reducer';
-
-export const rootReducer = combineReducers({
-  application,
-  editor,
-});
