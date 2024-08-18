931e9f51cc36e7bb02254bf56e38da75f07a8a4c
Jakub Antolak <poprostuantolak@gmail.com>
2024-08-18 13:21:25 +0200

    use shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <ModeToggle />
+        </div>
+        <div className="flex flex-col"></div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
