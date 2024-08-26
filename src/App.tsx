b23db46212fad43118e0d45569b74fd10c9fcbac
Jakub Antolak <poprostuantolak@gmail.com>
2024-08-26 06:46:35 +0200

    refactor for composition

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import ListContextProvider from "./state/list";
 import Container from "@/components/app/container";
 import SettingsPanel from "@/components/app/settings-panel";
 import View from "./components/app/view";
+import Artboard from "./components/app/view/artboard";
+import Defs from "./components/app/view/defs";
+import Figures from "./components/app/view/figures";
+import SizeControls from "./components/app/view/size-controls";
 
 function App() {
   return (
@@ -10,7 +14,13 @@ function App() {
       <ListContextProvider>
         <Container>
           <SettingsPanel />
-          <View />
+          <View>
+            <SizeControls />
+            <Artboard>
+              <Defs />
+              <Figures />
+            </Artboard>
+          </View>
         </Container>
       </ListContextProvider>
     </ThemeProvider>
--- src/components/app/view/index.tsx
+++ src/components/app/view/index.tsx
@@ -1,56 +1,9 @@
-import { Input } from "@/components/ui/input";
-import { useListContext } from "@/state/list";
-import { FC } from "react";
-import Artboard from "./artboard";
-import Defs from "./defs";
-import Figures from "./figures";
+import { FC, PropsWithChildren } from "react";
 
-const View: FC = () => {
-  const {
-    state: { artboardSize, gradients, globalBlendMode },
-    dispatch,
-  } = useListContext();
-  return (
-    <main className="flex flex-col h-full items-center justify-center relative">
-      <div className="flex items-center absolute z-10 top-2 p-4 bg-background rounded shadow-lg ">
-        <Input
-          type="number"
-          min={10}
-          step={1}
-          value={artboardSize[0]}
-          className="w-[100px]"
-          onChange={(event) => {
-            dispatch({
-              type: "SET_ARTBOARD_SIZE",
-              payload: {
-                size: [parseInt(event.target.value), artboardSize[1]],
-              },
-            });
-          }}
-        />
-        <span className="px-2">x</span>
-        <Input
-          type="number"
-          min={10}
-          step={1}
-          value={artboardSize[1]}
-          className="w-[100px]"
-          onChange={(event) => {
-            dispatch({
-              type: "SET_ARTBOARD_SIZE",
-              payload: {
-                size: [artboardSize[0], parseInt(event.target.value)],
-              },
-            });
-          }}
-        />
-      </div>
-      <Artboard>
-        <Defs gradients={gradients} globalBlendMode={globalBlendMode} />
-        <Figures />
-      </Artboard>
-    </main>
-  );
-};
+const View: FC<PropsWithChildren> = ({ children }) => (
+  <main className="flex flex-col h-full items-center justify-center relative">
+    {children}
+  </main>
+);
 
 export default View;
